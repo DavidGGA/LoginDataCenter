331d65fa04e94eabe08a25b060c6710f773a0416
dsvalenciah <dsvalenciah@unal.edu.co>
2017-07-10 11:43:15 -0500

    add twig render

--- validar.php
+++ validar.php
@@ -1,7 +1,24 @@
 <?php
-    $usuario=@$_POST['Usuario'];
-    $clave=@$_POST['Clave'];
+    require_once 'Twig-1.34.4\lib\Twig/Autoloader.php';
+    Twig_Autoloader::register();
+    $loader = new Twig_Loader_Filesystem('Templates');
+    $twig = new Twig_Environment($loader);
 
+    $str = "";
+    foreach ($_POST as $key => $value){
+        $str .= htmlspecialchars($key)." is ".htmlspecialchars($value);
+    }
+    $usuario=@$_POST['usuario'];
+    $clave=@$_POST['clave'];
+    /*
+    echo("
+        <html>
+            <script>
+                alert('".$str."')
+            </script>
+        </html>
+    ");
+    */
     //conectar a la base de datos
     $conexion=mysqli_connect("localhost", "root", "", "datacenter");
     $consulta="SELECT * FROM user WHERE useEmail= '$usuario' and usePassword='$clave'";
@@ -9,13 +26,15 @@
 
     $fila=mysqli_num_rows($resultado);
 
-    if ($fila>0){
-        while ($row = mysqli_fetch_row($resultado))
-        {
-            echo $row[0];
-        }
+    $user = mysqli_fetch_array($resultado);
+
+    if ($fila > 0){
+        echo($twig->render(
+            'profile.html',
+            array('email' => $user['useEmail'])
+        ));
     } else {
-        header("location:index.html");
+        echo($twig->render('home.html'));
     }
     mysqli_free_result($resultado);
     mysqli_close($conexion);
