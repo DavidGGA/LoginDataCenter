e466aa0bbac1759216c3524f12a2c0f281290842
juanset <jnset@hotmail.com>
2017-07-27 20:11:33 +0000

    htaccess

---
+++ DashBoard/Tools/Specs/application/views/vistas/headers.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang='es'>
+<head>
+	<title>Repositorio 1.0</title>
+	<meta charset="utf-8">
+	<meta charset="UTF-8" />
+		<!--<title><?php echo $titulo ?></title>-->
+
+</head>
+
+<body>
+
---
+++ DashBoard/Tools/Specs/application/views/vistas/mensajes.php
@@ -0,0 +1,7 @@
+<head>
+	<meta http-equiv="Refresh" content="1; url=http://maiadatacenter.co/specs/controlador2/mostrar_datos/">
+</head>
+<body>
+	<center><font><h1>¡Datos guardados con Exito!</h1></font></center>
+</body>
+</html>
