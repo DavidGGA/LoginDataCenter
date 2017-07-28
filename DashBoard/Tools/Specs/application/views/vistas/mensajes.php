981f3c663608da5266ac87b406c730accca3b4a3
dsvalenciah <dsvalenciah@unal.edu.co>
2017-07-28 11:24:35 -0500

    fixing fexing fexiado

--- DashBoard/Tools/Specs/application/views/vistas/mensajes.php
+++
@@ -1,7 +0,0 @@
-<head>
-	<meta http-equiv="Refresh" content="1; url=http://maiadatacenter.co/specs/controlador2/mostrar_datos/">
-</head>
-<body>
-	<center><font><h1>¡Datos guardados con Exito!</h1></font></center>
-</body>
-</html>
